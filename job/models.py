34b5bd76a382c2ba25e06effc64bcc7a89b5f820
Omar Bendary <omarbendary@Omars-MacBook-Pro.local>
2020-06-19 20:08:22 +0200

    job app models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Job(models.Model):
+
+    JOB_TYPE = [
+        ('Junior', 'Junior'),
+        ('Senior', 'Senior'),
+        ('Team Leader', 'Team Leader')
+    ]
+    title = models.CharField(max_length=50)
+    # loaction
+    job_type = models.CharField(choices=JOB_TYPE, max_length=50)
+    description = models.TextField(max_length=1000)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=0)
+    published_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
