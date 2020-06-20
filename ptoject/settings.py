6149f448cbac43d7e06940b13785754f3ee5cc12
Omar Bendary <omarbendary@Omars-MacBook-Pro.local>
2020-06-21 00:34:43 +0200

    static added

--- ptoject/settings.py
+++ ptoject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #my_apps
+    # my_apps
     'job',
     'home',
 ]
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+
+MEDIA_URL = '/media/'
