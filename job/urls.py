6a471681ed3a5a9651807215b3ebdc0e3986731a
Omar Bendary <omarbendary@Omars-MacBook-Pro.local>
2020-06-20 23:32:32 +0200

    views updated

--- job/urls.py
+++ job/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 
     path('', views.job_list),
-    path('<int:id>', views.job_list),
+    path('<int:id>', views.job_detail),
 ]
--- job/views.py
+++ job/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 
 
 def job_list(request):
-    pass
+    jobs = Job.objects.all()
+    context = {
+        'jobs': jobs
+    }
+    return render(request, 'job/job_list.html', context)
 
 
-def job_detail(request):
-    pass
+def job_detail(request, id):
+    job = Job.objects.get(id=id)
+    context = {
+        'job': job
+    }
+    return render(request, 'job/job_detail.html', context)
