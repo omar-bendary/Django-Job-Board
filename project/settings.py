8189c9a46ad6f0ba1df72a8bf82e904c477c14e1
Omar Bendary <omarbendary@Omars-MacBook-Pro.local>
2020-08-25 20:19:09 +0200

    none

--- project/settings.py
+++ project/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -141,5 +141,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'omar.bendary1996@gmail.com'
-EMAIL_HOST_PASSWORD = 'vtoqtochfvbagehr'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
